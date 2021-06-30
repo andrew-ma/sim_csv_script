7f5aa9b50d3861a158a185ffbe0229e7999642c9
Andrew Ma <anma@scires.com>
2021-06-30 16:19:43 -0500

    Add GUI requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 import setuptools
 
-with open("requirements.txt") as f:
-    required_packages = f.read().splitlines()
+requirements_files = ["requirements.txt", "requirements_gui.txt"]
+required_packages = []
+for req_filename in requirements_files:
+    with open(req_filename) as f:
+        required_packages.extend(f.read().splitlines())
 
 setuptools.setup(
     name="sim_csv_script",
     version="1.0.0",
     description="Program SIM cards by importing CSV file",
     packages=setuptools.find_packages(),
-    url="https://github.com/andrew-ma/sim_csv_script",
-    author="Bob",
-    author_email="test@test.com",
+    url="",
+    author="",
+    author_email="",
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
@@ -23,6 +26,7 @@ setuptools.setup(
     entry_points={
         "console_scripts": [
             "sim_csv_script = sim_csv_script.app:main",
+            "sim_csv_gui = sim_csv_gui.app:main"
         ]
     },
 )
